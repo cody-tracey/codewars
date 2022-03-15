e22b81664773dff0e46b69963c87d2cee8e24ed9
Cody <cody.a.tracey@gmail.com>
2022-03-15 09:15:13 -0500

    refactored to keep DRY and increase readability, have all but 1 test case working now

--- 17-pickPeaks.js
+++ 17-pickPeaks.js
@@ -20,17 +20,13 @@ const pickPeaks = arr => {
     if (arr.length === 0) return res;
 
     //Remove the first & last item(s) as we do not need them.
-    let temp = [...arr].reverse();
+    let temp = [...arr]
     let lastItem = arr[arr.length - 1];
     let firstItem = arr[0];
-    while (temp[0] === lastItem) {
-        temp.shift();
+    while (temp[0] === firstItem || temp[temp.length - 1] === lastItem) {
+        if (temp[0] === firstItem) temp.shift();
+        if (temp[temp.length - 1] === lastItem) temp.pop();
     };
-    temp = temp.reverse();
-    while (temp[0] === firstItem) {
-        temp.shift();
-    };
-
 
     arr = temp;
 
@@ -71,13 +67,13 @@ const pickPeaks = arr => {
 };
 
 //Test Cases:
-// console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
+console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
 console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
-// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
-// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
-// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
-// console.log(pickPeaks([]), { pos: [], peaks: [] });
-// console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
+console.log(pickPeaks([]), { pos: [], peaks: [] });
+console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
