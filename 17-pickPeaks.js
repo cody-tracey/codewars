784198c5c8f70ff5a904408d9a4bcc19d651eaa1
Cody <cody.a.tracey@gmail.com>
2022-03-15 14:44:27 -0500

    reworked logic of resetting the pos/peak, firing on all cylinders currently

--- 17-pickPeaks.js
+++ 17-pickPeaks.js
@@ -42,7 +42,7 @@ const pickPeaks = arr => {
         let next = arr[i + 1];
         let prev = arr[i - 1];
 
-        console.log(`I:${i}\tPrevious: ${prev}\tCurrent: ${curr}\tNext: ${next}\tPos:${pos}\tPeak:${peak}`);
+        // console.log(`I:${i}\tPrevious: ${prev}\tCurrent: ${curr}\tNext: ${next}\tPos:${pos}\tPeak:${peak}`);
 
         //If the current item is greater than the next item, and greater than the previous item we set it as the current peak/pos
         if (curr >= next && curr > prev) {
@@ -50,13 +50,13 @@ const pickPeaks = arr => {
             pos = i;
         }
         // If the current item is less than the previous and the next item we push the peak/pos to the res and 'reset' the peak/pos
-        else if (curr < prev && curr < next && peak !== undefined) {
+        else if (curr < prev && peak !== undefined) {
 
             res.pos.push(pos);
             res.peaks.push(peak);
 
-            peak = curr;
-            pos = i;
+            peak = undefined;
+            pos = undefined;
         };
 
         // If we are on the last item and we have not pushed this current peak/pos we do so
@@ -76,21 +76,21 @@ const pickPeaks = arr => {
 };
 
 //Test Cases:
-// console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
-// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
-// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
-// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
-// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
-// console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
-// console.log(pickPeaks([]), { pos: [], peaks: [] });
-// console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
-// console.log(pickPeaks([14, -1, 8, 4, 0, 2, 2, 0, 11, 5]), { pos: [2, 5, 8], peaks: [8, 2, 11] });
-// console.log(pickPeaks([9, 0, 6]), { pos: [], peaks: [] });
-// console.log(pickPeaks([0, 13, 15, 3, 9, 9, -3, 9, -3, 7, 3, 2, 9, -2, 10, 1, -1, -3, 1, 1, 2, 12, 15]), { pos: [2, 4, 7, 9, 12, 14], peaks: [15, 9, 9, 7, 9, 10] });
-// console.log(pickPeaks([7, 2, -1, 15, 0, 14, 2, 4, 6, 8, 4, -1, 5, -2, -1, 8, 5, 9, -1, 3, 4, 10, 14, 8, -1, 6, 3, 14, -1, 6]), { pos: [3, 5, 9, 12, 15, 17, 22, 25, 27], peaks: [15, 14, 8, 5, 8, 9, 14, 6, 14] });
-console.log(pickPeaks([-3, 6, 5, 0, 0, 11, -3, 11, -1, 7, -3, 3, 14, 7, 13, 14, 8, 0, 12, 10, 12, -2, 7, 2]), { pos: [1, 5, 7, 9, 12, 15, 18, 20, 22], peaks: [6, 11, 11, 7, 14, 14, 12, 12, 7] });
+console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
+console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
+console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
+console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
+console.log(pickPeaks([]), { pos: [], peaks: [] });
+console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
+console.log(pickPeaks([14, -1, 8, 4, 0, 2, 2, 0, 11, 5]), { pos: [2, 5, 8], peaks: [8, 2, 11] });
+console.log(pickPeaks([9, 0, 6]), { pos: [], peaks: [] });
+console.log(pickPeaks([0, 13, 15, 3, 9, 9, -3, 9, -3, 7, 3, 2, 9, -2, 10, 1, -1, -3, 1, 1, 2, 12, 15]), { pos: [2, 4, 7, 9, 12, 14], peaks: [15, 9, 9, 7, 9, 10] });
+console.log(pickPeaks([7, 2, -1, 15, 0, 14, 2, 4, 6, 8, 4, -1, 5, -2, -1, 8, 5, 9, -1, 3, 4, 10, 14, 8, -1, 6, 3, 14, -1, 6]), { pos: [3, 5, 9, 12, 15, 17, 22, 25, 27], peaks: [15, 14, 8, 5, 8, 9, 14, 6, 14] });
+// console.log(pickPeaks([-3, 6, 5, 0, 0, 11, -3, 11, -1, 7, -3, 3, 14, 7, 13, 14, 8, 0, 12, 10, 12, -2, 7, 2]), { pos: [1, 5, 7, 9, 12, 15, 18, 20, 22], peaks: [6, 11, 11, 7, 14, 14, 12, 12, 7] });
 
 
 
