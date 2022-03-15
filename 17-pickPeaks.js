6fbdfd80ca4ef7dd7fcd493f33c46b665499df34
Cody <cody.a.tracey@gmail.com>
2022-03-15 08:28:59 -0500

    solved for plateau peaks at the start or middle of the code, but have trouble when it ends on a plateau, will need to slice off the end chars rather than just pop

--- 17-pickPeaks.js
+++ 17-pickPeaks.js
@@ -20,10 +20,8 @@ const pickPeaks = arr => {
     arr.pop();
 
     for (let i = 1; i < arr.length; i++) {
-        console.log(`
-            
-        `)
-        if (arr[i] > arr[i + 1] && arr[i] > arr[i-1]) {
+        // console.log(`I:${i}\tPrevious: ${arr[i-1]}\tCurrent: ${arr[i]}\tNext: ${arr[i+1]}`)
+        if (arr[i] >= arr[i + 1] && arr[i] > arr[i-1]) {
             res.pos.push(i)
             res.peaks.push(arr[i])
         }
@@ -33,13 +31,13 @@ const pickPeaks = arr => {
 };
 
 //Test Cases:
-console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
-console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
-console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
-console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
+// console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
+// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
+// console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
+// console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
 console.log(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
-console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
-console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
-console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
-console.log(pickPeaks([]), { pos: [], peaks: [] });
-console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
+// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
+// console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
+// console.log(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
+// console.log(pickPeaks([]), { pos: [], peaks: [] });
+// console.log(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
