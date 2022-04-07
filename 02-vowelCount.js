bf1da34943a6fdc7fa7c06a2f1180a8692bce60a
Cody <cody.a.tracey@gmail.com>
2022-04-07 08:48:52 -0500

    refactored 02 to use a reduce function and remove unnecessary vars

--- 02-vowelCount.js
+++ 02-vowelCount.js
@@ -5,32 +5,54 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 */
 
+
+//Original
+// const getCount = str => {
+//     //Count variable
+//     let vowelsCount = 0;
+
+//     //Split to use arr methods
+//     let strArr = str.split('');
+
+//     //Loop over it and check to see if it is a vowel and if so increment the count
+//     strArr.map(i => {
+//         switch (i) {
+//             case 'a':
+//             case 'e':
+//             case 'i':
+//             case 'o':
+//             case 'u':
+//             vowelsCount++;
+//             break;
+//         };
+//     });
+
+//     return vowelsCount;
+// };
+
+//Refactored
 const getCount = str => {
-    //Count variable
-    let vowelsCount = 0;
-
-    //Split to use arr methods
-    let strArr = str.split('');
-
-    //Loop over it and check to see if it is a vowel and if so increment the count
-    strArr.map(i => {
-        switch (i) {
+     /*
+    Split into an array and then reduce the array to a single number.
+    */
+    return str.split('').reduce((total, curr) => {
+        switch (curr) {
             case 'a':
             case 'e':
             case 'i':
             case 'o':
             case 'u':
-            vowelsCount++;
-            break;
+                total++
+                break;
         };
-    });
-
-    return vowelsCount;
+        return total
+    }, 0)
 };
 
 
+
 //Test Cases:
-console.log(getCount("abracadabra"), 5)
-console.log(getCount("pear tree"), 4)
-console.log(getCount("o a kak ushakov lil vo kashu kakao"), 13)
-console.log(getCount("my pyx"), 0)
+console.log(getCount("abracadabra") === 5)
+console.log(getCount("pear tree") === 4)
+console.log(getCount("o a kak ushakov lil vo kashu kakao") === 13)
+console.log(getCount("my pyx") === 0)
