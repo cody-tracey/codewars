5dec404e4ed0216cb88284aefa9a3b1dd93ed9f7
Cody <cody.a.tracey@gmail.com>
2022-04-14 09:22:50 -0500

    added in my forEach that will break the pieces up into their own sub arrays

--- 25-textAlignJustify.js
+++ 25-textAlignJustify.js
@@ -46,43 +46,81 @@ lacus,   ut   elementum  justo
 nulla et dolor.
 */
 
-const justify = (text,width) => {
-    
+const justify = (text, width) => {
+    let textArr = text.split(' ');
+    let resArr = [];
+
+
+    textArr.forEach((i, index) => {
+        let l = resArr[resArr.length - 1];
+        if (index === 0) {
+            resArr.push([i])
+        }
+        else if (l.join(' ').length + i.length + 1 <= width) {
+            l.push(i)
+        }
+        else {
+            resArr.push([i])
+        }
+    });
+
+
+    // let r = resArr.map((i,index) => {
+    //     if (index !== resArr.length-1) {
+    //         let holder = i.pop();
+    //         let l = width - holder.length;
+    //         let c = 0;
+    //         while (i.join(' ').length < l) {
+                
+    //             i[c % i.length] += ' ';
+    //             c++
+    //         };
+    //         i.push(holder);
+    //         return i
+    //     }
+    //     else {
+    //     return i            
+    //     }
+
+    // })
+
+    return resArr
 }
 
 //Test Cases:
 
-console.log(justify('123 45 6', 7), '123  45\n6');
-console.log(justify('123', 7), '123');
-console.log(justify('', 10), '');
+console.log(justify('123 45 6', 7), '\n123  45\n6');
+// console.log(justify('123', 7), '123');
+// console.log(justify('', 10), '');
 
 const LIPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.';
 
-console.log(justify(LIPSUM, 30), `Lorem  ipsum  dolor  sit amet,
-consectetur  adipiscing  elit.
-Vestibulum    sagittis   dolor
-mauris,  at  elementum  ligula
-tempor  eget.  In quis rhoncus
-nunc,  at  aliquet orci. Fusce
-at   dolor   sit   amet  felis
-suscipit   tristique.   Nam  a
-imperdiet   tellus.  Nulla  eu
-vestibulum    urna.    Vivamus
-tincidunt  suscipit  enim, nec
-ultrices   nisi  volutpat  ac.
-Maecenas   sit   amet  lacinia
-arcu,  non dictum justo. Donec
-sed  quam  vel  risus faucibus
-euismod.  Suspendisse  rhoncus
-rhoncus  felis  at  fermentum.
-Donec lorem magna, ultricies a
-nunc    sit    amet,   blandit
-fringilla  nunc. In vestibulum
-velit    ac    felis   rhoncus
-pellentesque. Mauris at tellus
-enim.  Aliquam eleifend tempus
-dapibus. Pellentesque commodo,
-nisi    sit   amet   hendrerit
-fringilla,   ante  odio  porta
-lacus,   ut   elementum  justo
-nulla et dolor.`);
+// console.log(justify(LIPSUM, 30), 
+// `Lorem  ipsum  dolor  sit amet,
+// consectetur  adipiscing  elit.
+// Vestibulum    sagittis   dolor
+// mauris,  at  elementum  ligula
+// tempor  eget.  In quis rhoncus
+// nunc,  at  aliquet orci. Fusce
+// at   dolor   sit   amet  felis
+// suscipit   tristique.   Nam  a
+// imperdiet   tellus.  Nulla  eu
+// vestibulum    urna.    Vivamus
+// tincidunt  suscipit  enim, nec
+// ultrices   nisi  volutpat  ac.
+// Maecenas   sit   amet  lacinia
+// arcu,  non dictum justo. Donec
+// sed  quam  vel  risus faucibus
+// euismod.  Suspendisse  rhoncus
+// rhoncus  felis  at  fermentum.
+// Donec lorem magna, ultricies a
+// nunc    sit    amet,   blandit
+// fringilla  nunc. In vestibulum
+// velit    ac    felis   rhoncus
+// pellentesque. Mauris at tellus
+// enim.  Aliquam eleifend tempus
+// dapibus. Pellentesque commodo,
+// nisi    sit   amet   hendrerit
+// fringilla,   ante  odio  porta
+// lacus,   ut   elementum  justo
+// nulla et dolor.`);
